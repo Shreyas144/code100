6061da0c3267fd7a63c1167fe0c66dff93664b2b
Shreyas144 <shreyas144@protonmail.com>
2021-10-27 22:48:24 +0530

    added project files

---
+++ PR-001/1.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a,b,c;
+    cin>>a>>b>>c;
+    cout<<"MEDIA = "<<setprecision(2)<<(2*a+3*b+5*c)/10<<endl;
+
+    return 0;
+}
