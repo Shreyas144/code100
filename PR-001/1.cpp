8516c4d332d941d51c5250b28a93fdd378624f8f
Shreyas144 <shreyas144@protonmail.com>
2021-12-07 19:53:39 +0530

    Commit of 7/12/2021

--- PR-001/1.cpp
+++ PR-001/1.cpp
@@ -1,11 +1,38 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main()
-{
-    float a,b,c;
-    cin>>a>>b>>c;
-    cout<<"MEDIA = "<<setprecision(2)<<(2*a+3*b+5*c)/10<<endl;
+// }
+// int main()
+// {
+//     float a,b,c;
+//     cin>>a>>b>>c;
+//     cout<<"MEDIA = "<<setprecision(2)<<(2*a+3*b+5*c)/10<<endl;
 
-    return 0;
-}
+//     return 0;
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// void print(map<int, string> &m )
+// {
+//     cout<< m.size() <<endl;
+//     for(auto &pr :m)
+//     {
+//         cout<<pr.first<<" "<<pr.second<<endl;
+    
+//     }
+// }
+
+// int main()
+// {
+//     map<int, string> m;
+//     m[1]="abc";
+//     m[5]="bsj";
+//     m[3]="csj";
+//     m.insert({4,"jwd"});
+//     m[6];
+//     print(m);
+
+
+//     return 0;
+// }
---
+++ PR-002/2.cpp
@@ -0,0 +1,19 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int t;
+
+//     while(t--){
+//         int n;
+//         cin>>n;
+//         int a[i];
+//         for(int i=0;i<a.size;a++){
+//             for(int )
+//         }
+
+//     }
+
+//     return 0;
+// }
