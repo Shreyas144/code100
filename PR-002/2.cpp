70177822bf1e3a426f53b695b7bcf20cc5825e11
Shreyas144 <shreyas144@protonmail.com>
2021-12-09 19:22:21 +0530

    9/12/2021

--- PR-002/2.cpp
+++ PR-002/2.cpp
@@ -17,3 +17,22 @@
 
 //     return 0;
 // }
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void main(){
+//     int n;
+//     cin>> n;
+//     for(int i=1;i<n+1;i++){
+//         for(int j=1;j<<=i;j++){
+//             if((n-j)!=0){
+//                 cout<< " "*(n-j) ;
+                                
+//             }
+//             cout<<
+
+//         }
+//     }
+// }
